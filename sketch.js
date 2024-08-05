144a1bba49b5c8ef8adf1ff5d4692c3f22224130
emilysilv3 <00001092811643sp@al.educacao.sp.gov.br>
2024-08-05 17:02:53 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  
+   createCanvas(1000, 800);
+  background("rgba(221,18,53,0.88)");
+}
+function draw() {
+fill ("#00BCD4");
+  stroke ("#9C27B0");
+  if (mouseIsPressed)
+  
+  circle (mouseX,mouseY,60);
+  
+ }
